73244501e4db283ced8b2601f519f15c0110504d
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-06-10 11:06:41 -0400

    Added to schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,3 +5,24 @@ CREATE DATABASE employee_trackerDB;
 --Using new database
 USE employee_trackerDB;
 
+--Creating tables
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+)
+
