431b936509dbf6d9e51b16fea01dabdadc213cbc
Stephanie <st3phanie.harris@gmail.com>
2023-06-11 00:19:14 -0400

    Added and formatted seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
---Seeds for database
+--Seeds for database 
 --Department
 INSERT INTO
     department (name) VALUE ('Sales');
@@ -13,22 +13,26 @@ INSERT INTO
     department (name) VALUE ('Legal');
 
 --Role
-INSERT INTO
-    role (title, salary, department_id) VALUE ('Sales Lead', 70000, 1),
-    ('Salesperson', 50000, 1),
-    ('Lead Engineer', 130000, 2),
-    ('Account Manager', 70000, 3),
-    ('Accountant', 100000, 3),
-    ('Legal Team Lead', 500000, 4),
-    ('Lawyer', 350000, 4);
+INSERT INTO role (title, salary, department_id) 
+  VALUE ('Sales Lead', 70000, 1),
+        ('Salesperson', 50000, 1),
+        ('Lead Engineer', 130000, 2),
+        ('Account Manager', 70000, 3),
+        ('Accountant', 100000, 3),
+        ('Legal Team Lead', 500000, 4),
+        ('Lawyer', 350000, 4);
 
 --Employees
-INSERT INTO
-    employee (first_name, last_name, role_id, manager_id) VALUE ('Sylvanas', 'Windrunner', 1, NULL),
-    ('Vanessa', 'VanCleef', 2, 1),
-    ('Lady', 'Liadrin', 2, 1),
-    ('Draca', 'Orc', 2, 1),
-    ('Lady', 'Vashj', 3, NULL),
-    ('Valeera', 'Sanguinar', 4, NULL),
-    ('Alexstrasza', 'Dragonflight', 4, 1),
-    ('Jaina', 'Proudmoore', 3, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+  VALUE ('Sylvanas', 'Windrunner', 1, NULL),
+        ('Vanessa', 'VanCleef', 2, 1),
+        ('Lady', 'Liadrin', 2, 1),
+        ('Draca', 'Orc', 1, 1),
+        ('Lady', 'Vashj', 3, NULL),
+        ('Valeera', 'Sanguinar', 4, NULL),
+        ('Alexstrasza', 'Dragonflight', 4, 1),
+        ('Jaina', 'Proudmoore', 3, 2);
+        ('Grommash', 'Hellscream', 1, 2),
+        ('Tyrande', 'Whisperwind', 1, NULL),
+        ('Veressa', 'Windrunner', 4, 2),
+        ('Malfurion', 'Stormrage', 3, NULL);
